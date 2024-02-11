4b387dcff2a9e13f70c1dfb183a8ac2df27e5957
Jonseen <onajijohnson1@gmail.com>
2024-02-11 19:34:05 +0100

    Updated Complete website

--- circo.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector("#menu");
-
-const dropmenu = menu.addEventListener("click", function(){
-   let release = document.querySelector(".navbar");
-   
-   release.classList.toggle("drop");
-})
-
-// menu change to x
